30db2eba850dc05a7ff39053185ecd1dea4f2a1f
katy tran <katychi.tran@gmail.com>
2022-10-28 08:23:13 -0700

    add homescreen

--- 2do_fe/src/App.js
+++ 2do_fe/src/App.js
@@ -1,8 +1,42 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Date from "./components/Date/Date";
+import TaskDetail from "./components/TaskDetail/TaskDetail";
 
 function App() {
-  return <NavBar />;
+  return (
+    <div>
+      <NavBar />
+      <React.Fragment>
+        <Container maxWidth={false} style={{ marginTop: "50px" }}>
+          <div id="content">
+            <div style={{ textAlign: "center", border: "1px solid" }}>
+              <Box sx={{ flexGrow: 1 }}>
+                <div class="grid grid-cols-5 gap-2">
+                  <div className="text-3xl py-5 justify-self-start pl-14 col-span-2">
+                    Task
+                  </div>
+                  <div className="text-3xl py-5 ">Due Date</div>
+                  <div className="text-3xl py-5 justify-self-end pr-14 col-span-2">
+                    Stage
+                  </div>
+                </div>
+                <Date />
+                <TaskDetail />
+                <TaskDetail />
+                <TaskDetail />
+                <TaskDetail />
+                <TaskDetail />
+              </Box>
+            </div>
+          </div>
+        </Container>
+      </React.Fragment>
+    </div>
+  );
 }
 
 export default App;
--- 2do_fe/src/components/NavBar/NavBar.js
+++ 2do_fe/src/components/NavBar/NavBar.js
@@ -3,19 +3,29 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    neutral: {
+      main: "#EEECF1",
+      contrastText: "black",
+    },
+  },
+});
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit" component="div">
-            2Do
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static" color="neutral">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit" component="div">
+              2Do
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
     </Box>
   );
 }
