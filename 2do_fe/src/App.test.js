f7fd832b21278cb41d565fbf91ef1670649cbafb
Walter Toriola <walte@Walters-MBP.lan>
2022-11-03 13:02:30 -0700

    Added test to fe and be

--- 2do_fe/src/App.test.js
+++ 2do_fe/src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders the Task text", () => {
   render(<App />);
 
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const Task = screen.getByText("Task");
+  expect(Task).toBeDefined();
 });
