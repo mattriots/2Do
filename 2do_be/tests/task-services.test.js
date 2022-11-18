5a505d6bef1a2007c4e85f05fd8a19ded8ebb9e0
Matt <mattdepauw@gmail.com>
2022-11-17 17:13:08 -0800

    mroe to do

--- 2do_be/tests/task-services.test.js
+++
@@ -1,8 +0,0 @@
-const taskServices = require("../models/task-services");
-
-
-test(("Get all tasks"), async () => {
-  const target = 0;
-  const result = await taskServices.getTasks();
-  expect(result).toBeDefined();
-});
