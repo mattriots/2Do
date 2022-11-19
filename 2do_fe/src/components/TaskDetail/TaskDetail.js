ce72a187afeed18d9a349c84cc47ec8ab0ec5a39
katy tran <katychi.tran@gmail.com>
2022-11-18 19:42:15 -0800

    add delete icon when hover a task

--- 2do_fe/src/components/TaskDetail/TaskDetail.js
+++ 2do_fe/src/components/TaskDetail/TaskDetail.js
@@ -4,6 +4,7 @@ import ProgressIcon from "./ProgressIcon.svg";
 import FinishIcon from "./FinishIcon.svg";
 import StageButton from "../Done/StageButton.js";
 import { openEditDesc } from "../PopUps/EditTaskPopUp";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 //import { openTaskDesc } from "../AddTask/AddTaskPopUp.js";
 import Moment from "react-moment";
 
@@ -50,8 +51,15 @@ export default function TaskDetail({ task }) {
           </div>
         </div>
         {isShownDelete ? (
-          <div className="py-2 justify-self-end self-center pr-14 col-span-2">
-            Delete Icon
+          <div className="py-2 justify-self-end col-span-2 flex">
+            <div className="pr-4">
+              {task.status === "completed"
+                ? StageButton("complete", "Done")
+                : StageButton("inprogress", "In Progress")}
+            </div>
+            <div className="pr-5">
+              <RemoveCircleIcon />
+            </div>
           </div>
         ) : (
           <div className="py-2 justify-self-end pr-14 col-span-2">
